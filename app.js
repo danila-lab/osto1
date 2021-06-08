186a3fcb71dedc3b5f327fc086789f65302e26d0
DP2DGolcovs <>
2021-06-08 12:53:37 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+function openH() {
+    document.location.href = "../index.html";
+}
+function openR(l) {
+	if (l == 1) {
+		document.location.href = "reservation/reservations.html";
+	} else {
+		document.location.href = "../reservation/reservations.html";
+	}
+}
+function openC(l) {
+	if (l == 1) {
+		document.location.href = "contacts/contacts.html";
+	} else {
+		document.location.href = "../contacts/contacts.html";
+	}
+}
+function openA(l) {
+	if (l == 1) {
+		document.location.href = "about/about.html";
+	} else {
+		document.location.href = "../about/about.html";
+	}
+}
+function openP(l) {
+	if (l == 1) {
+		document.location.href = "login/login.html";
+	} else {
+		document.location.href = "../login/login.html";
+	}
+}
+function openN(l) {
+	if (l == 1) {
+		document.location.href = "notifications/notifications.html";
+	} else {
+		document.location.href = "../notifications/notifications.html";
+	}
+}
